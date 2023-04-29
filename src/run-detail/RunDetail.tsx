5316a7250b8551c127a364ddd90ad6f07dc35056
Rodrigo Alonso <rodrigoignacioalonso@gmail.com>
2023-04-29 13:47:34 -0400

    Add delete option to RunDetail screen

--- src/run-detail/RunDetail.tsx
+++ src/run-detail/RunDetail.tsx
@@ -1,14 +1,48 @@
-import { RouteProp, useRoute } from '@react-navigation/native';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { RunsStackParamList } from '../main-tab-navigator';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
+import React, { useCallback, useContext } from 'react';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+import {
+  RunsStackParamList,
+  RunsStackScreenProps,
+} from '../main-tab-navigator';
 import { RunnerDivider } from '../ui-components';
 import { useRunDetailData } from '../data/useRunDetailData';
+import { RunDataReducerAction } from '../data/RunDataProvider';
+import { RunDataContext } from '../data/RunDataProvider';
+import { getRun } from '../data/storage/getRun';
+import { Run } from '../data/storage/getRuns';
 
 export function RunDetail() {
+  const navigation = useNavigation<RunsStackScreenProps['navigation']>();
   const route = useRoute<RouteProp<RunsStackParamList, 'RunDetail'>>();
   const { runId } = route.params;
   const run = useRunDetailData(runId);
+  const { dispatch: runDataDispatch } = useContext(RunDataContext);
+
+  const onPressDelete = useCallback(() => {
+    if (run === undefined) {
+      return;
+    }
+    const runToDelete = getRun(run._id) as Run;
+
+    Alert.alert('Delete run', 'Are you sure you want to delete this run?', [
+      {
+        text: 'Cancel',
+        style: 'cancel',
+      },
+      {
+        text: 'Delete',
+        onPress: () => {
+          runDataDispatch({
+            action: RunDataReducerAction.Delete,
+            data: runToDelete,
+          });
+          navigation.goBack();
+        },
+        style: 'destructive',
+      },
+    ]);
+  }, [navigation, run, runDataDispatch]);
 
   if (run === undefined) {
     return (
@@ -19,18 +53,21 @@ export function RunDetail() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Distance</Text>
-      <Text style={styles.value}>{run.distance}</Text>
-      <RunnerDivider />
-      <Text style={styles.title}>Duration</Text>
-      <Text style={styles.value}>{run.duration}</Text>
-      <RunnerDivider />
-      <Text style={styles.title}>Pace</Text>
-      <Text style={styles.value}>{run.pace}</Text>
-      <RunnerDivider />
-      <Text style={styles.title}>Date</Text>
-      <Text style={styles.value}>{run.date}</Text>
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.title}>Distance</Text>
+        <Text style={styles.value}>{run.distance}</Text>
+        <RunnerDivider />
+        <Text style={styles.title}>Duration</Text>
+        <Text style={styles.value}>{run.duration}</Text>
+        <RunnerDivider />
+        <Text style={styles.title}>Pace</Text>
+        <Text style={styles.value}>{run.pace}</Text>
+        <RunnerDivider />
+        <Text style={styles.title}>Date</Text>
+        <Text style={styles.value}>{run.date}</Text>
+      </View>
+      <Button title="Delete" color="red" onPress={onPressDelete} />
     </View>
   );
 }
