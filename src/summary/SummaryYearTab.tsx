3c44fad2146b35249df1b1cfb88d1bad0f6b340a
Rodrigo Alonso <rodrigoignacioalonso@gmail.com>
2023-05-03 19:19:33 -0400

    Add existing summary data to SummaryYearTab

--- src/summary/SummaryYearTab.tsx
+++ src/summary/SummaryYearTab.tsx
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { useRuns } from '../data/useRuns';
-import { RunnerPicker, RunnerPickerOption } from '../ui-components';
+import {
+  RunnerDivider,
+  RunnerPicker,
+  RunnerPickerOption,
+} from '../ui-components';
 import { useNavigation } from '@react-navigation/native';
+import { useSummaryDisplayData } from './useSummaryDisplayData';
 
 export function SummaryYearTab() {
   const navigation = useNavigation();
@@ -11,13 +16,15 @@ export function SummaryYearTab() {
   const maxYear = runs[0].date.getFullYear();
   const minYear = runs[runs.length - 1].date.getFullYear();
 
-  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
+  const [selectedYear, setSelectedYear] = useState(maxYear);
   const [yearPickerOpen, setYearPickerOpen] = useState(false);
 
   const options: RunnerPickerOption<number>[] = [];
   for (let i = maxYear; i >= minYear; i--) {
     options.push({ key: i.toString(), label: i.toString(), value: i });
   }
+
+  const summaryDisplayData = useSummaryDisplayData(selectedYear);
 
   useEffect(() => {
     const unsubscribe = navigation.addListener('blur', () => {
@@ -29,11 +36,17 @@ export function SummaryYearTab() {
 
   return (
     <View style={styles.container}>
-      <Text>{selectedYear}</Text>
-      <Button
-        title="Select year"
-        onPress={() => setYearPickerOpen(!yearPickerOpen)}
-      />
+      <Pressable onPress={() => setYearPickerOpen(!yearPickerOpen)}>
+        <Text style={styles.pickerValue}>{selectedYear}</Text>
+      </Pressable>
+      <View style={styles.summaryContainer}>
+        <Text style={styles.title}>Total distance</Text>
+        <Text style={styles.value}>{summaryDisplayData.totalDistance}</Text>
+        <RunnerDivider />
+        <Text style={styles.title}>Average duration</Text>
+        <Text style={styles.value}>{summaryDisplayData.avgDuration}</Text>
+      </View>
+
       <RunnerPicker
         isOpen={yearPickerOpen}
         options={options}
@@ -51,5 +64,23 @@ export function SummaryYearTab() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  pickerValue: {
+    fontSize: 24,
+    paddingLeft: 20,
+    paddingTop: 20,
+  },
+  summaryContainer: {
+    backgroundColor: 'white',
+    margin: 16,
+    borderRadius: 12,
+    padding: 16,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: '200',
+  },
+  value: {
+    fontSize: 20,
   },
 });
