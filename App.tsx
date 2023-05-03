1ed1c75712f3fb2e1cb5e665c69520a5ee147dec
Rodrigo Alonso <rodrigoignacioalonso@gmail.com>
2023-05-03 18:22:36 -0400

    Install bottom-sheet for date picker in summary tabs

--- App.tsx
+++ App.tsx
@@ -2,15 +2,25 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { RootStackNavigator } from './src/root-stack-navigator';
 import { RunDataProvider } from './src/data/RunDataProvider';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { StyleSheet } from 'react-native';
 
 function App(): JSX.Element {
   return (
-    <NavigationContainer>
-      <RunDataProvider>
-        <RootStackNavigator />
-      </RunDataProvider>
-    </NavigationContainer>
+    <GestureHandlerRootView style={styles.container}>
+      <NavigationContainer>
+        <RunDataProvider>
+          <RootStackNavigator />
+        </RunDataProvider>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
