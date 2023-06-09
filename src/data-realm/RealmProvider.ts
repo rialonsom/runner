d8442c05ee3d1792309549288815f3b4fd8dadd0
Rodrigo Alonso <rodrigoignacioalonso@gmail.com>
2023-06-09 19:06:36 -0400

    Add setting for skipping schema migration for faster realm testing

--- src/data-realm/RealmProvider.ts
+++ src/data-realm/RealmProvider.ts
@@ -4,6 +4,8 @@ import { Run } from './run/runModel';
 
 const realmConfig: Realm.Configuration = {
   schema: [Run],
+  // Uncomment if making changes to schema in order to test fast
+  // deleteRealmIfMigrationNeeded: true,
 };
 
 export const { RealmProvider, useObject, useQuery, useRealm } =
