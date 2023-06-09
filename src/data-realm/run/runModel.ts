3ec04ad7f8ead6b07bfa1084b3c4c57b5044db08
Rodrigo Alonso <rodrigoignacioalonso@gmail.com>
2023-06-09 16:58:06 -0400

    Add realm Run data model

---
+++ src/data-realm/run/runModel.ts
@@ -0,0 +1,19 @@
+import Realm from 'realm';
+
+export class Run extends Realm.Object<Run> {
+  _id!: Realm.BSON.ObjectId;
+  durationSeconds!: number;
+  distanceMeters!: number;
+  date!: Date;
+
+  static schema = {
+    name: 'Run',
+    properties: {
+      _id: 'objectId',
+      durationSeconds: 'int',
+      distanceMeters: 'int',
+      date: 'date',
+    },
+    primaryKey: '_id',
+  };
+}
