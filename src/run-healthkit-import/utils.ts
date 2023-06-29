5e70ce8ad971c33d1edc98c1806dc4fd1cc743e0
Rodrigo Alonso <rodrigoignacioalonso@gmail.com>
2023-06-29 16:58:18 -0400

    Add util to get imported run props from healthkit runs

---
+++ src/run-healthkit-import/utils.ts
@@ -0,0 +1,15 @@
+import { ImportedRunProps, RunSource } from '../data-realm/run/runModel';
+import { HealthkitRun } from './types';
+
+export function getRunsPropsFromHealthkitRuns(
+  healthkitRuns: HealthkitRun[],
+): ImportedRunProps[] {
+  const runsProps = healthkitRuns.map(run => ({
+    durationSeconds: run.durationSeconds,
+    distanceMeters: run.distanceMeters,
+    date: new Date(run.timeIntervalSinceEpoch),
+    source: RunSource.Healthkit,
+  }));
+
+  return runsProps;
+}
