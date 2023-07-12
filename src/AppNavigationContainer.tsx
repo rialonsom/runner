5d6719939c79468cb602463a4c255cc64e84efb3
Rodrigo Alonso <rodrigoignacioalonso@gmail.com>
2023-07-12 15:23:40 -0400

    Add BottomSheetModalProvider to app nav container

--- src/AppNavigationContainer.tsx
+++ src/AppNavigationContainer.tsx
@@ -2,13 +2,16 @@ import React, { useContext } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { RootStackNavigator } from './root-stack-navigator';
 import { ThemeContext } from './theme';
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 
 export function AppNavigationContainer() {
   const { navigationTheme } = useContext(ThemeContext);
 
   return (
     <NavigationContainer theme={navigationTheme}>
-      <RootStackNavigator />
+      <BottomSheetModalProvider>
+        <RootStackNavigator />
+      </BottomSheetModalProvider>
     </NavigationContainer>
   );
 }
