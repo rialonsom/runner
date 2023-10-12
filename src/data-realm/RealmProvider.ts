df048d766136e2f61d57d15cd74f520530848f5e
Rodrigo Alonso <rodrigoignacioalonso@gmail.com>
2023-10-11 22:34:49 -0300

    Add Shoe model to schema

--- src/data-realm/RealmProvider.ts
+++ src/data-realm/RealmProvider.ts
@@ -1,9 +1,10 @@
 import Realm from 'realm';
 import { createRealmContext } from '@realm/react';
 import { Run } from './run/runModel';
+import { Shoe } from './shoe/shoeModel';
 
 const realmConfig: Realm.Configuration = {
-  schema: [Run],
+  schema: [Run, Shoe],
   // Uncomment if making changes to schema in order to test fast
   // deleteRealmIfMigrationNeeded: true,
 };
