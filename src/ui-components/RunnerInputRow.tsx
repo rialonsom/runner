6fb9ab5a69846a6f56fbe46897c7a381e2473e68
Rodrigo Alonso <rodrigoignacioalonso@gmail.com>
2023-10-13 22:54:39 -0300

    Add disabled prop to RunnerInputRow

--- src/ui-components/RunnerInputRow.tsx
+++ src/ui-components/RunnerInputRow.tsx
@@ -3,12 +3,13 @@ import { StyleSheet, TouchableOpacity, View } from 'react-native';
 
 type RunnerInputRowProps = {
   children: React.ReactNode;
+  disabled?: boolean;
   onPress?: () => void;
 };
 
 export function RunnerInputRow(props: RunnerInputRowProps) {
   return (
-    <TouchableOpacity onPress={props.onPress}>
+    <TouchableOpacity onPress={props.onPress} disabled={!!props.disabled}>
       <View style={[styles.container]}>{props.children}</View>
     </TouchableOpacity>
   );
