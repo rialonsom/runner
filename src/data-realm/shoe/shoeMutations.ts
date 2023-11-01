10d9de9f02dfb0839cff0839668273c89e4ac620
Rodrigo Alonso <rodrigoignacioalonso@gmail.com>
2023-11-01 17:53:20 -0300

    Add mutation to assign shoe to shoeless runs

--- src/data-realm/shoe/shoeMutations.ts
+++ src/data-realm/shoe/shoeMutations.ts
@@ -1,14 +1,19 @@
 import Realm from 'realm';
 import uuid from 'react-native-uuid';
 import { Shoe, ShoeProps } from './shoeModel';
+import { getRunsWithinDates } from '../run/runQueries';
 
-export function addShoe(newShoeProps: ShoeProps, realm: Realm) {
+export function addShoe(newShoeProps: ShoeProps, realm: Realm): string {
+  const _id = uuid.v4().toString();
+
   realm.write(() => {
     realm.create('Shoe', {
       ...newShoeProps,
-      _id: uuid.v4().toString(),
+      _id,
     });
   });
+
+  return _id;
 }
 
 export function updateShoe(shoe: Shoe, newShoeProps: ShoeProps, realm: Realm) {
@@ -24,5 +29,19 @@ export function updateShoe(shoe: Shoe, newShoeProps: ShoeProps, realm: Realm) {
 export function deleteShoe(shoe: Shoe, realm: Realm) {
   realm.write(() => {
     realm.delete(shoe);
+  });
+}
+
+export function applyShoeToRunsWithinDates(shoe: Shoe, realm: Realm) {
+  if (!shoe.endDate) {
+    return;
+  }
+
+  const { startDate, endDate } = shoe;
+  const runs = getRunsWithinDates(startDate, endDate, realm);
+  const runsWithoutShoe = runs.filter(run => !run.shoe[0]);
+
+  realm.write(() => {
+    shoe.runs.push(...runsWithoutShoe);
   });
 }
