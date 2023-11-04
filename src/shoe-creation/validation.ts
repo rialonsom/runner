675620122ae58bfc29dd5b75a76fa92343c3aaaf
Rodrigo Alonso <rodrigoignacioalonso@gmail.com>
2023-11-04 14:57:49 -0300

    Replace shoe creation validation switch with if chain
    - switch fall through beahavior executes the next case regardless it it's true, which is not what we want for this use case

--- src/shoe-creation/validation.ts
+++ src/shoe-creation/validation.ts
@@ -14,18 +14,17 @@ export function validateShoeInput(shoeInput: ShoeInput): {
   const { brand, name, startDate, hasEndDate, endDate, lifespan } = shoeInput;
 
   let errors: string[] = [];
-  switch (true) {
-    case brand.length <= 0:
-      errors.push("- Brand can't be empty.");
-    // falls through
-    case name.length <= 0:
-      errors.push("- Name can't be empty.");
-    // falls through
-    case hasEndDate && endDate.getTime() - startDate.getTime() <= 0:
-      errors.push('- Start date should be earlier than End date.');
-    // falls through
-    case Number.isNaN(parseInt(lifespan, 10)) || parseInt(lifespan, 10) < 0:
-      errors.push("- Lifespan can't be empty.");
+  if (brand.length <= 0) {
+    errors.push("- Brand can't be empty.");
+  }
+  if (name.length <= 0) {
+    errors.push("- Brand can't be empty.");
+  }
+  if (hasEndDate && endDate.getTime() - startDate.getTime() <= 0) {
+    errors.push('- Start date should be earlier than End date.');
+  }
+  if (Number.isNaN(parseInt(lifespan, 10)) || parseInt(lifespan, 10) < 0) {
+    errors.push("- Lifespan can't be empty.");
   }
 
   return {
